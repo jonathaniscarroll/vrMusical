ce3def62910628bbd10222fd82b0be035a07c6f7
Jonathan Carroll <jonathaniscarroll@gmail.com>
2017-10-07 23:08:03 -0400

    brandscape unity prototype

---
+++ vrMusicalUnity/Assets/reduceFog.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class reduceFog : MonoBehaviour {
+
+	public GameObject target;
+	private float startDist;
+	private float startDens;
+
+	// Use this for initialization
+	void Start () {
+		startDens = RenderSettings.fogDensity;
+		startDist = Vector3.Distance (target.transform.position,transform.position);
+		StartCoroutine (clearFog ());
+	}
+
+	 IEnumerator clearFog(){
+		float dist = Vector3.Distance (target.transform.position,transform.position);
+		while (dist > .10f) {
+			dist = Vector3.Distance (target.transform.position,transform.position);
+			RenderSettings.fogDensity = dist / startDist * startDens;
+			Debug.Log ("dist: " + dist + " " + dist / startDist * startDens);
+			yield return null;
+		}
+	}
+}
---
+++ vrMusicalUnity/Assets/waypointMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class waypointMove : MonoBehaviour {
+
+	public GameObject allPoints;
+	private List<Vector3> wp;
+	private int index;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		wp = new List<Vector3> ();
+		index = 0;
+		foreach (Transform child in allPoints.transform) {
+			wp.Add (child.position);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (transform.position != wp [index]) {
+			transform.position = Vector3.MoveTowards (transform.position, wp [index], Time.deltaTime * speed);
+		} else {
+			if (index >= wp.Count) {
+				index = 0;
+			} else {
+				index++;
+			}
+		}
+	}
+}
